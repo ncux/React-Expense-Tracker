70fae9eaa144e20f08d8e6b68eb360355f9c70c3
Talent Malaba <malabatalent@gmail.com>
2020-02-24 14:28:45 +0800

    UI done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Header } from "./components/header/header";
+import { Balance } from "./components/balance/balance";
+import { IncomeExpense } from "./components/income-expense/income-expense";
+import { TransactionList } from "./components/transactions-list/transaction-list";
+import { AddTransaction } from "./components/add-transaction/add-transaction";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header />
+      <div className="container">
+          <Balance />
+          <IncomeExpense />
+          <TransactionList />
+          <AddTransaction />
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/balance/balance.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const Balance = props => {
+
+    return (
+        <>
+            <h4>Your balance</h4>
+            <h1>$0.00</h1>
+        </>
+    );
+
+};
+
